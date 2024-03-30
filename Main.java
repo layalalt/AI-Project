dcf8f5ebc0da02ff8756b70de022038cae2f0c87
layalalt <147754472+layalalt@users.noreply.github.com>
2024-03-30 21:06:50 +0300

    Add files via upload
    اتوقع هذا افضل

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main
 
     public static void main(String[] args) 
     {
-        int[][] grid = new int[4][10]; //10 by 4
+       /** int[][] grid = new int[4][10]; //10 by 4
         Random random = new Random();
 
         for(int i=0; i<10; i++) //fills up last row
@@ -166,7 +166,113 @@ public class Main
                     grid[2][i] = -1;
              }
           }    
-        }       
+        }   */
+        
+        
+        int[][] grid = new int[4][10]; //10 by 4
+        Random random = new Random();
+        
+          for(int i=0; i<3; i++) //initializer
+            for(int j=0; j<10; j++) 
+                grid[i][j] = -1;
+           
+         
+        long startTime = System.currentTimeMillis(), endTime = 1000000; //to prevent infinite loops
+        boolean infiniteLoop = false;  
+           
+        for(int i=0; i<3; i++) 
+        {
+          for(int j=0; j<10; j++)
+          {
+                endTime = System.currentTimeMillis();
+                
+                if((endTime - startTime)/1000.0 > 2)
+                {    
+                    infiniteLoop = true;
+                    break;
+                
+                }
+                
+                int num = random.nextInt(10);
+                boolean exists = false;
+                for(int k=j; k>=0; k--) //checks if it exists already in row; ensures uniqueness
+                {
+                 if(num == grid[i][k])
+                   exists = true;
+                }
+                if(i == 1) //second row
+                {
+                  if(j == 0)
+                  {
+                   if((num == grid[0][0]) || (num == grid[0][1])) //first element
+                     exists = true;
+                  }
+                  else if(j == 9)
+                  {  
+                   if((num == grid[0][8]) || (num == grid[0][9])) //last element  
+                     exists = true;
+                  }
+                  else if (j>0 && j<9)
+                  {  
+                   if((num == grid[0][j - 1]) || (num == grid[0][j]) || (num == grid[0][j + 1]))
+                     exists = true; 
+                  }     
+                }      
+                else if(i == 2) 
+                {
+                 if(j == 0)
+                 {
+                  if((num == grid[1][0]) || (num == grid[1][1])) //first element
+                     exists = true;
+                 }
+                 else if(j == 9)
+                 {  
+                   if((num == grid[1][8]) || (num == grid[1][9])) //last element  
+                     exists = true;
+                 }
+                  else if (j>0 && j<9)
+                  {  
+                   if((num == grid[1][j - 1]) || (num == grid[1][j]) || (num == grid[1][j + 1]))
+                     exists = true; 
+                  }  
+                } 
+                if(exists)
+                  j--;
+                else
+                  grid[i][j] = num;
+          }
+          if((endTime - startTime)/1000.0 > 5)
+                    break;
+        }
+        
+           for(int i=0; i<10; i++) 
+           {    
+               grid[3][i] = grid[0][i] + grid[1][i] + grid[2][i];
+               if(infiniteLoop)
+               {
+                 if(grid[1][i] == -1)
+                     grid[3][i] = grid[0][i];
+                 else if(grid[2][i] == -1)
+                    grid[3][i] = grid[0][i] + grid[1][i];
+                 else
+                     grid[3][i] = grid[0][i] + grid[1][i] + grid[2][i];            
+               }
+           }
+        
+        for(int i=0; i<3; i++) 
+        {
+          for(int j=0; j<10; j++)
+          {
+             boolean result = random.nextDouble() < 0.5; //generates probability
+             if(infiniteLoop)
+                result = random.nextDouble() < 0.7; 
+             if(result) //fill grid index
+                grid[i][j] = -1;
+          }
+        }
+        
+        
+        
        
         for(int i=0; i<3; i++)
         {
